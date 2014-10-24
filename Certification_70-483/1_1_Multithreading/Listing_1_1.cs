7889cc340287e9206ff1c2dc8b6edf5a08a78058
Adrien Buet <adrien.buet@gmail.com>
2014-10-24 11:26:21 +0200

    Project creation
    New Vution and creation of .gitignore

---
+++ Certification_70-483/1_1_Multithreading/Listing_1_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1_1_Multithreading
+{
+    using System.Threading;
+
+    class Listing_1_1
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(1);
+            }
+        }
+
+        public static void Run()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(1);
+            }
+            t.Join();
+        }
+    }
+}
---
+++ Certification_70-483/1_1_Multithreading/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1_1_Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Listing_1_1.Run();
+        }
+    }
+}

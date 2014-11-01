16b448c834983305e1be3e0767a13a2f116095f3
Adrien Buet <adrien.buet@gmail.com>
2014-11-01 20:15:56 +0100

    Listing 1.18 to 1.27

---
+++ Certification_70-483/1_1_Multithreading/Multithreading/Plinq/Listing_1_23.cs
@@ -0,0 +1,22 @@
+﻿using Certification.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certification.Chapter1.Multithreading.Plinq
+{
+    public class Listing_1_23 : IRunnable
+    {
+        public void Run()
+        {
+            var numbers = Enumerable.Range(0, 10);
+            var parallelResult = numbers.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+            .Where(i => i % 2 == 0)
+            .ToArray();
+            foreach (int i in parallelResult)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ Certification_70-483/1_1_Multithreading/Multithreading/Plinq/Listing_1_27.cs
@@ -0,0 +1,34 @@
+﻿using Certification.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certification.Chapter1.Multithreading.Plinq
+{
+    public class Listing_1_27 : IRunnable
+    {
+        public void Run()
+        {
+            var numbers = Enumerable.Range(0, 20);
+            try
+            {
+                var parallelResult = numbers.AsParallel()
+                .Where(i => IsEven(i));
+                parallelResult.ForAll(e => Console.WriteLine(e));
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("There where {0} exceptions",
+                e.InnerExceptions.Count);
+            }
+        }
+
+        public static bool IsEven(int i)
+        {
+            if (i % 10 == 0) throw new ArgumentException("i");
+            return i % 2 == 0;
+        }
+    }
+}

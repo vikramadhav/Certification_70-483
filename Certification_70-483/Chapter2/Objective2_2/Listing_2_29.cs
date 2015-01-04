8e4ef0fb3ef8b324bdc60c9a843f1b957658afc3
Adrien Buet <adrien.buet@gmail.com>
2015-01-04 16:47:28 +0100

    Objective 2.2

---
+++ Certification_70-483/Chapter2/Objective2_2/Listing_2_29.cs
@@ -0,0 +1,45 @@
+﻿using Certification.Common;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certification.Chapter2.Objective2_2
+{
+    public class Listing_2_29 : IRunnable
+    {
+        public void Run()
+        {
+            dynamic obj = new SampleObject();
+            obj.SomeProperty = "test";
+            obj.AnotherProperty = 67;
+            Console.WriteLine(obj.SomeProperty);
+            Console.WriteLine(obj.AnotherProperty);
+            Console.WriteLine((string)obj.PropertyThatDoesNotExist); 
+        }
+
+        public class SampleObject : DynamicObject
+        {
+            private Dictionary<string, object> members = new Dictionary<string, object>();
+
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                if (!members.TryGetValue(binder.Name, out result))
+                {
+                    result = null;
+                }
+
+                return true;
+            }
+
+            public override bool TrySetMember(SetMemberBinder binder, object value)
+            {
+                members[binder.Name] = value;
+                return true;
+            }
+        }
+
+    }
+}
---
+++ Certification_70-483/Chapter2/Objective2_2/Listing_2_30.cs
@@ -0,0 +1,20 @@
+﻿using Certification.Common;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certification.Chapter2.Objective2_2
+{
+    public class Listing_2_30 : IRunnable
+    {
+        public void Run()
+        {
+            dynamic test = new ExpandoObject();
+            test.NewProperty = "toto";
+            Console.WriteLine(test.NewProperty);
+        }
+    }
+}
